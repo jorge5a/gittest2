a1e3ba71f869583847c7649fe9f46dd9dea8345c
jorge5a <jorge@ciclopesl.com>
2023-03-09 15:56:52 +0100

    a todos de la rama2

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hola a todos de la rama!");
+        System.out.println("Hola a todos de la rama con cambios!");
     }
 }
